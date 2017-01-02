b69f0bbf13a5a83708e6aa3471160c4a7083852b
David Stern <dbstern@login4.colonialone.gwu.edu>
2017-01-02 17:28:43 -0500

    working

--- get_reference_transcripts_from_rbbh.py
+++ get_reference_transcripts_from_rbbh.py
@@ -14,34 +14,38 @@ print 'Indexing reference...'
 ref_index = SeqIO.index(reference, "fasta")
 print 'Extracting sequences'
 
+count_rbbh = 0
 blastout = open(infile, 'rU')
 for line in blastout:
-    count_rbbh == 0
+    rec_count = 0
     seq1 = line.split('\t')[0]
-    species1 = sequence.split('@')[0]
+    species1 = seq1.split('@')[0]
     seq2 = line.split('\t')[1]
-    species2 = sequence.split('@')[1]
+    species2 = seq2.split('@')[0]
     output1 = open(species1+'.rbbh.fasta', 'a')
     output2 = open(species2+'.rbbh.fasta', 'a')
-    gene1 = '_'.join(sequence1.split('_')[:-1])
-    gene2 = '_'.join(sequence2.split('_')[:-1])
+    gene1 = '_'.join(seq1.split('_')[:-1])
+    gene2 = '_'.join(seq2.split('_')[:-1])
     trans1 = []
+    trans2 = []
     for record in ref_index.keys():
         if gene1 in record and record not in trans1: 
             trans1.append(record)
     for record in ref_index.keys():
         if gene2 in record and record not in trans2: 
-            trans1.append(record)
+            trans2.append(record)
     records1 = (ref_index[tran] for tran in trans1)
     records2 = (ref_index[tran] for tran in trans2)
     for record in records1:
-        record.id = 'RBBH'+str(count_rbbh)+'_'+str(records1.index(record))
-        record.description = 'RBBH'+str(count_rbbh)+'_'+str(records1.index(record))
+        record.id = 'RBBH'+str(count_rbbh)+'_'+str(rec_count)
+        record.description = 'RBBH'+str(count_rbbh)+'_'+str(rec_count)
         SeqIO.write(record, output1, "fasta")
+        rec_count += 1
     for record in records2:
-        record.id = 'RBBH'+str(count_rbbh)+'_'+str(records2.index(record))
-        record.description = 'RBBH'+str(count_rbbh)+'_'+str(records2.index(record))
-        SeqIO.write(record, output1, "fasta")
+        record.id = 'RBBH'+str(count_rbbh)+'_'+str(rec_count)
+        record.description = 'RBBH'+str(count_rbbh)+'_'+str(rec_count)
+        SeqIO.write(record, output2, "fasta")
+        rec_count += 1
     count_rbbh += 1
             
         
